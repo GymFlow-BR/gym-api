006ae496080a3a45d6b7e12100ab4cef74027fdc
SKDEV9 <skgomes06@gmail.com>
2026-06-26 18:37:39 -0300

    fix: protect users endpoint by HTTP method

--- src/main/java/br/com/gymflow/api/config/security/SecurityConfig.java
+++ src/main/java/br/com/gymflow/api/config/security/SecurityConfig.java
@@ -42,7 +42,13 @@ public class SecurityConfig {
                         .requestMatchers(HttpMethod.POST, "/api/users")
                         .hasAnyRole("ADMIN", "TEACHER")
 
-                        .requestMatchers("/api/users/**")
+                        .requestMatchers(HttpMethod.GET, "/api/users", "/api/users/**")
+                        .hasRole("ADMIN")
+
+                        .requestMatchers(HttpMethod.PATCH, "/api/users/**")
+                        .hasRole("ADMIN")
+
+                        .requestMatchers(HttpMethod.DELETE, "/api/users/**")
                         .hasRole("ADMIN")
 
                         .requestMatchers(HttpMethod.GET, "/api/students/**")
